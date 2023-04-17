6409acc77424ef369c74cc9ee44b1bb8f9c0ff2a
root <emilykamiti@gmail.com>
2023-04-17 23:55:59 +0000

    mit

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,28 +1,3 @@
 import { taskFirst, taskNext } from './0-constants.js';
 
 console.log(`${taskFirst()} ${taskNext()}`);
-
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
--- INSERT --                                           0,1           All
-
