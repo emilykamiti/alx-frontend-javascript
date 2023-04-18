e2b446bb2a9ef1b2e9f574dea35aa6304358ee98
root <emilykamiti@gmail.com>
2023-04-18 14:00:07 +0000

    mit

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,46 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-next-line no-unused-vars
+    /}
+
+  return [task, task2];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
     const task = true;
-    // eslint-disable-next-line no-unused-vars
     const task2 = false;
   }
 
