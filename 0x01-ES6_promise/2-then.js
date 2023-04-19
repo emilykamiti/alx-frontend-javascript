34bbe9a4e7595c46f6adbd1c100406f663dce8b3
root <emilykamiti@gmail.com>
2023-04-19 23:36:49 +0000

    git

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+// eslint-disable no-unreachable
+
+export default function handleResponseFromAPI(Promise) {
+  return Promise.then(
+    () => ({
+      status: 200,
+      body: 'success',
+    }),
+  ).catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
