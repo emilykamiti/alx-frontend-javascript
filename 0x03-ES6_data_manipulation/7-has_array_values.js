14eef56f5e078bc7a666908c50f282d85ba9de56
root <emilykamiti@gmail.com>
2023-04-24 11:55:30 +0000

    init

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,9 @@
+/* eslint-disable guard-for-in */
+export default function hasValuesFromArray(set, array) {
+  for (const prop of array) {
+    if (!set.has(prop)) {
+      return false;
+    }
+  }
+  return true;
+}
